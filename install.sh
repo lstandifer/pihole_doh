7c43f9ea60bf9bb73d9dc1c8cbe2cdce4cd33e47
lstandifer <lstandifer@users.noreply.github.com>
2021-12-13 00:46:39 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# install basic packages
+apt-get -y update \
+    && apt-get -y dist-upgrade \
+    && apt-get -y install bash
+
+# install cloudflared
+
+cd /tmp \
+    && wget wget -O cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 \
+    && cp ./cloudflared /usr/local/bin \
+    && rm -f ./cloudflared-stable-linux-arm.tgz
+useradd -s /usr/sbin/nologin -r -M cloudflared \
+    && chown cloudflared:cloudflared /usr/local/bin/cloudflared
+    
+# clean cloudflared config
+mkdir -p /etc/cloudflared \
+    && rm -f /etc/cloudflared/config.yml
+
+# add cloudflared config
+cd /tmp \
+    && mkdir -p /etc/cloudflared \
+    && cp -n ./config.yaml /etc/cloudflared/ \
+    && rm -f ./config.yaml
+
+# run cloudflared as service
+mkdir -p /etc/services.d/cloudflared \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/run \
+    && echo 's6-echo "Starting cloudflared"' >> /etc/services.d/cloudflared/run \
+    && echo '/usr/local/bin/cloudflared --config /etc/cloudflared/config.yaml' >> /etc/services.d/cloudflared/run \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/finish \
+    && echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflared/finish \
+    && echo 'killall -9 cloudflared' >> /etc/services.d/cloudflared/finish
+	
+# clean up
+apt-get -y autoremove \
+    && apt-get -y autoclean \
+    && apt-get -y clean \
+    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
